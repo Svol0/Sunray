5e3dc8eb8a9b26bfbc0b3da8cecfdfe2024383a6
Svol0 <90195294+Svol0@users.noreply.github.com>
2022-06-27 20:12:25 +0200

    Update config_example.h

--- sunray/config_example.h
+++ sunray/config_example.h
@@ -446,8 +446,8 @@ Also, you may choose the serial port below for serial monitor output (CONSOLE).
 #define DOCK_IGNORE_GPS false     // use GPS fix in docking station and IMU for GPS float/invalid
 //#define DOCK_IGNORE_GPS true     // ignore GPS fix in docking station and use IMU-only (use this if robot gets false GPS fixes in your docking station)
 
-#define DOCK_SLOW_ONLY_LAST_POINTS  4 // Svol0: dockingpoint number (counted from last dockingpoint) where slow speed will be used to reach the dockingstation (0 = all points will be reached with slow speed)
-#define DOCK_POINT_GPS_REBOOT       4 // Svol0: dockingpoint number (counted from last dockingpoint) where the gps will be rebooted and waited for gps-fix by undocking. 0 = no gps reboot by undocking
+#define DOCK_SLOW_ONLY_LAST_POINTS  2 // Svol0: dockingpoint number (counted from last dockingpoint) where slow speed will be used to reach the dockingstation (0 = all points will be reached with slow speed)
+#define DOCK_POINT_GPS_REBOOT       2 // Svol0: dockingpoint number (counted from last dockingpoint) where the gps will be rebooted and waited for gps-fix by undocking. 0 = no gps reboot by undocking
 
 //#define DOCK_AUTO_START true     // robot will automatically continue mowing after docked automatically
 #define DOCK_AUTO_START false      // robot will not automatically continue mowing after docked automatically
