338630c73c12a9a47636fce4538bcd4117c4179b
Svol0 <90195294+Svol0@users.noreply.github.com>
2022-07-15 08:33:41 +0200

    update for bl-driver

--- sunray/src/driver/AmRobotDriver.h
+++ sunray/src/driver/AmRobotDriver.h
@@ -11,14 +11,42 @@
 #include <Arduino.h>
 #include "RobotDriver.h"
 
+struct DriverChip {
+    char const *driverName;       // name of driver (MC33926 etc.)
+    bool forwardPwmInvert;  // forward pin uses inverted pwm?
+    bool forwardDirLevel;   // forward pin level
+    bool reversePwmInvert;  // reverse pin uses inverted pwm?
+    bool reverseDirLevel;   // reverse pin level
+    bool usePwmRamp;        // use a ramp to get to PWM value?
+    bool faultActive;       // level for fault active (LOW/HIGH)
+    bool resetFaultByToggleEnable; // reset a fault by toggling enable? 
+    bool enableActive;      // level for enable active (LOW/HIGH)
+    bool disableAtPwmZeroSpeed; // disable driver at PWM zero speed? (brake function)
+    bool keepPwmZeroSpeed;  // keep zero PWM value (disregard minPwmSpeed at zero speed)?    
+    int minPwmSpeed;        // minimum PWM speed to ensure     
+    int maxPwmSpeed;        // maximum PWM speed to ensure     
+    byte pwmFreq;           // PWM frequency (PWM_FREQ_3900 or PWM_FREQ_29300)
+    float adcVoltToAmpOfs;  // ADC voltage to amps (offset)     // current (amps)= ((ADCvoltage + ofs)^pow) * scale
+    float adcVoltToAmpScale; // ADC voltage to amps (scale)
+    float adcVoltToAmpPow;   // ADC voltage to amps (power of number)   
+    //bool drivesMowingMotor; // drives mowing motor?    
+};
 
 class AmRobotDriver {
   public:
     void begin();
     void run();
 };
-
-
+/*
+class AmRobotDriver: public RobotDriver {
+  public:
+    void begin() override;
+    void run() override;
+    bool getRobotID(String &id) override;    
+    bool getMcuFirmwareVersion(String &name, String &ver) override;
+    float getCpuTemperature() override;
+};
+*/
 
 class AmMotorDriver: public MotorDriver {
   public:    
@@ -31,8 +59,21 @@ class AmMotorDriver: public MotorDriver {
     void getMotorCurrent(float &leftCurrent, float &rightCurrent, float &mowCurrent) override;
     void getMotorEncoderTicks(int &leftTicks, int &rightTicks, int &mowTicks) override;
   protected:
-    void setMC33926(int pinDir, int pinPWM, int speed);
-    void setBrushless(int pinDir, int pinPWM, int speed);
+    int lastLeftPwm;
+    int lastRightPwm;
+    int lastMowPwm;
+    int leftSpeedSign; // current motor direction (1 or -1)
+    int rightSpeedSign; // current motor direction (1 or -1)
+    int mowSpeedSign; // current motor direction (1 or -1)
+    DriverChip MC33926;
+    DriverChip DRV8308;
+    DriverChip A4931;
+    DriverChip BLDC8015A;
+    DriverChip JYQD;
+    DriverChip CUSTOM;
+    DriverChip mowDriverChip;
+    DriverChip gearsDriverChip;
+    void setMotorDriver(int pinDir, int pinPWM, int speed, DriverChip &chip, int speedSign);    
 };
 
 class AmBatteryDriver : public BatteryDriver {
